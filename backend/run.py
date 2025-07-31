983ae3198b3f72d1439ab175a57546814c11ecde
Sukhnoor-K <skhehra@ualberta.ca>
2025-07-30 21:05:27 -0600

    Add gunicorn import and render deployment support

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # default fallback is 10000
+    app.run(host="0.0.0.0", port=port) # set host to 0.0.0.0 for Render deployment
